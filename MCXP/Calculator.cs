8f756ed12d4da154e2e6a46246f150aea9179934
asdia0 <asdia.the.potato@gmail.com>
2021-06-13 18:59:13 +0800

    Add documentation

--- MCXP/Calculator.cs
+++ MCXP/Calculator.cs
@@ -3,7 +3,12 @@
     using System;
 
     public static class Calculator
-    {        
+    {
+        /// <summary>
+        /// Converts the number of XP to levels and points.
+        /// </summary>
+        /// <param name="currentXP">The amount of XP.</param>
+        /// <returns>The levels and points equivalent to the XP given.</returns>
         public static (int levels, int points) GetLevelsAndPoints(int currentXP)
         {
             for (int i = 0; i <= 15; i++)
@@ -52,16 +57,33 @@
             }
         }
 
+        /// <summary>
+        /// Adds a certain amount of XP and then converts the total XP to levels and points.
+        /// </summary>
+        /// <param name="currrentXP">Initial amount of XP.</param>
+        /// <param name="diff">Amount of XP to add.</param>
+        /// <returns>The levels and points equivalent to the total XP.</returns>
         public static (int levels, int points) AddXP(int currrentXP, int diff)
         {
             return GetLevelsAndPoints(currrentXP + diff);
         }
 
+        /// <summary>
+        /// Subtracts a certain amount of XP and then converts the total XP to levels and points.
+        /// </summary>
+        /// <param name="currrentXP">Initial amount of XP.</param>
+        /// <param name="diff">Amount of XP to subtract.</param>
+        /// <returns>The levels and points equivalent to the total XP.</returns>
         public static (int levels, int points) SubtractXP(int currentXP, int diff)
         {
             return GetLevelsAndPoints(currentXP - diff);
         }
 
+        /// <summary>
+        /// Converts levels and points to the number of XP.
+        /// </summary>
+        /// <param name="currentXP">The levels and points.</param>
+        /// <returns>The number of XP equivalent to the levels and points given.</returns>
         public static long LevelsAndPointsToXP(int levels, int points)
         {
             if (levels < 0)
@@ -82,6 +104,12 @@
             return (int)((4.5 * levels * levels) - (162.5 * levels) + 2220 + points);
         }
 
+        /// <summary>
+        /// Calculates the XP difference between two different levels and points.
+        /// </summary>
+        /// <param name="currentXP">Initial levels and points.</param>
+        /// <param name="diff">New levels and points.</param>
+        /// <returns>Difference in XP.</returns>
         public static long XPDiff((int levels, int points) currentXP, (int levels, int points) diff)
         {
             return LevelsAndPointsToXP(diff.levels, diff.points) - LevelsAndPointsToXP(currentXP.levels, currentXP.points);
